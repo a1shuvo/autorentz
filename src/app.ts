e36b954ff4000c9b01d8226c8c8ffe68d2173925
a1shuvo <a1shuvosaha@gmail.com>
2025-12-05 21:08:59 +0600

    Initial project setup

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import initDB from "./config/db";
+
+const app = express();
+
+// parser
+app.use(express.json());
+// app.use(express.urlencoded());
+
+// initializing DB
+initDB();
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello Next Level Developers!");
+// });
+
+
+// Handle undefined routes
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found!",
+    path: req.path,
+  });
+});
+
+export default app;
